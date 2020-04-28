cc60763133b02fa877d28ea6eb74f0dd476f2e07
Matthieu Huin <mhuin@redhat.com>
2020-04-28 14:13:06 +0200

    [WIP] user CRUD with keycloak
    Change-Id: I457b03f0316770021f65b62fe9bc2855875b1f42

--- sfmanager/sfmanager.py
+++ sfmanager/sfmanager.py
@@ -24,6 +24,8 @@ import re
 import git
 import requests
 import sqlite3
+import shlex
+import subprocess
 import sys
 import time
 try:
@@ -285,6 +287,9 @@ def sf_user_management_command(parser):
                         required=True, help="The user's full name")
     create.add_argument('--email', '-e', nargs='?', metavar='email',
                         required=True, help="The user's email")
+    create.add_argument('--ssh-key', '-s', nargs='?',
+                        metavar='/path/to/pubkey',
+                        required=False, help="The user's ssh public key file")
     sfu_sub.add_parser('list', help='list all registered users')
     delete = sfu_sub.add_parser('delete', help='de-register a user from SF')
     delete.add_argument('--username', '-u', nargs='?', metavar='username',
@@ -559,12 +564,47 @@ def github_action(args, base_url):
     return False
 
 
-@fail_if_keycloak
 def user_management_action(args, base_url):
     if args.command != 'user':
         return False
     if args.subcommand not in ['create', 'update', 'delete']:
         return False
+    services = sfauth.get_managesf_info(args.url)['service']['services']
+    if 'keycloak' in services:
+        return keycloak_user_management_action(args, base_url)
+    else:
+        return cauth_user_management_action(args, base_url)
+
+
+def keycloak_user_management_action(args, base_url):
+    if args.subcommand in ['create']:
+        password = None
+        if args.password is None:
+            # -p option has been passed by with no value
+            password = getpass.getpass("Enter password: ")
+        elif args.password:
+            password = args.password
+        userInfo = {"username": args.username}
+        if getattr(args, 'email'):
+            userInfo['email'] = args.email
+        # TODO make sure it works
+        if getattr(args, 'ssh_key'):
+            with open(args.ssh_key, 'r') as f:
+                userInfo['attributes'] = {"publicKey": [f.read()]}
+        if getattr(args, 'fullname'):
+            userInfo['firstName'] = args.fullname[0]
+            if len(args.fullname) > 1:
+                userInfo['lastName'] = args.fullname[1]
+        if password is not None:
+            userInfo['credentials'] = [{"type": "password",
+                                        "value": password}]
+        base = args.url.rstrip('/')
+        url = base + 'auth/admin/realms/sf/users'
+        resp = request('post', url, json=userInfo)
+        return response(resp)
+
+
+def cauth_user_management_action(args, base_url):
     url = build_url(base_url, 'user', args.username)
     if args.subcommand in ['create', 'update']:
         password = None
@@ -641,12 +681,7 @@ def project_action(args, base_url):
     return True
 
 
-@fail_if_keycloak
-def services_users_management_action(args, base_url):
-    if args.command != 'sf_user':
-        return False
-    if args.subcommand not in ['create', 'list', 'delete']:
-        return False
+def cauth_services_users_management_action(args, base_url):
     url = build_url(base_url, 'services_users')
     if args.subcommand in ['create', 'delete']:
         info = {}
@@ -674,6 +709,44 @@ def services_users_management_action(args, base_url):
         else:
             return response(resp)
     return response(resp)
+
+
+def keycloak_services_users_management_action(args, base_url):
+    if args.subcommand == 'create':
+        cmd = "ssh gerrit gerrit set-account --active"
+        if getattr(args, 'fullname'):
+            cmd += " --full-name \"'%s'\"" % args.fullname
+        if getattr(args, 'email'):
+            cmd += " --add-email %s" % args.email
+        if getattr(args, 'password'):
+            cmd += " --http-password %s" % args.password
+        if getattr(args, 'username'):
+            cmd += " %s" % args.username
+        try:
+            env = os.environ.copy()
+            env['LC_ALL'] = 'en_US.UTF-8'
+            output = subprocess.check_output(
+                shlex.split(cmd), stderr=subprocess.STDOUT,
+                env=env).decode('utf-8')
+        except subprocess.CalledProcessError as err:
+            if err.output:
+                die(err.output)
+            else:
+                die(err)
+        print(output)
+        return True
+
+
+def services_users_management_action(args, base_url):
+    if args.command != 'sf_user':
+        return False
+    if args.subcommand not in ['create', 'list', 'delete']:
+        return False
+    services = sfauth.get_managesf_info(args.url)['service']['services']
+    if 'keycloak' in services:
+        return keycloak_services_users_management_action(args, base_url)
+    else:
+        return cauth_services_users_management_action(args, base_url)
 
 
 def main():
